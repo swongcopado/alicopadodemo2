62c340cdcd5f6185a695d05bb17284e0db959ecd
Trial User One <swong@copado.com>
2023-08-31 11:10:39 +0000

    US-0000045: PoCForAli_wave1_trial6

---
+++ lwc/pOCCustomerLWC/pOCCustomerLWC.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class POCCustomerLWC extends LightningElement {
+    content = "Hello World";
+    constructor() {
+        super();
+    }
+}
